03e000b81e60feff921a70c01fd5a4b47a19c5d4
Samuel JS Wright <swrig109@uncc.edu>
2023-09-25 11:55:22 -0400

    docstring

--- modelingutils/model_setup.py
+++ modelingutils/model_setup.py
@@ -1,13 +1,37 @@
+from numpy import isnan
+
+
 def check_Xy_for_na(X, y):
+    """Checks for missing values in the target variable and features
+
+    Parameters
+    ----------
+    X : pandas.DataFrame or numpy.ndarray
+
+    y : pandas.Series or numpy.ndarray
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    AssertionError
+        If there are missing values in the target variable
+
+    Exception
+        If there are missing values in the features and the user chooses not to continue
+    """
+
     try:
         print("Checking for missing values in the target variable")
-        assert y.isna().sum() == 0
+        assert isnan(y).sum() == 0
     except AssertionError:
         print("There are missing values in the target variable")
 
     try:
         print("Checking for missing values in the features")
-        assert (X.isna().sum() == 0).all()
+        assert isnan(X).sum().sum() == 0
     except:
         print(
             "There are missing values in the features, if this is expected enter 'y' to continue"
